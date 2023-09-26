96c468c70d5073f5907ae5fd210e9c03f8ad5ce6
Luca Giorgino <19236900+lucagiorgino@users.noreply.github.com>
2023-09-26 11:59:56 +0200

    Update config and .env to support SEPOLIA

--- hardhat.config.js
+++ hardhat.config.js
@@ -47,6 +47,10 @@ module.exports = {
       gas: 2100000, 
       gasPrice: 8000000000,
       accounts: [process.env.PRIVATE_KEY_ISSUER],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY_ISSUER]
     }
   },
   etherscan: {
