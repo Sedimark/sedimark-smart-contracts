c0e8fcb7b179d0ed0146b5822dca06275b803800
Luca Giorgino <19236900+lucagiorgino@users.noreply.github.com>
2023-10-31 17:21:15 +0100

    Add script

---
+++ init.bat
@@ -0,0 +1,9 @@
+npx hardhat --network localhost faucet 0xF9692336D7f37336C2061a545D8b2895B1415EFe & ^
+npx hardhat --network localhost faucet 0xACC7CE50Be9D1db2eF86f804F5bC03b53681Fcbe & ^
+npx hardhat --network localhost faucet 0xBB3fb731CC4b103C072594E226A396F84C3E5DF4 & ^
+npx hardhat --network localhost faucet 0x25B2E1942578bCBB1Aa6E8EAa038DfE73440fcbe & ^
+npx hardhat --network localhost faucet 0xAba0719cc3E1187A4C9Caf6773D9766541C2Dd8D & ^
+npx hardhat --network localhost faucet 0xd0300C2817624BDd6C34F1d8e44857046dE13110 & ^
+npx hardhat --network localhost faucet 0xD12270A9054d045ba276C16Cb1C64386cDAA6AC7 & ^
+npx hardhat --network localhost faucet 0x6d3fbD514D62E2D305e4435dDe99483f20f264BF & ^
+npx hardhat run --network hardhat-issuer scripts/deploy.js
